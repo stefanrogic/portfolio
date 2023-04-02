fec404ce73150ec3526f37a30f3f55bc8dceedb6
rasgrin <stefanrogic@protonmail.com>
2023-04-02 17:02:01 +0200

    changed base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./portfolio",
+  base: "/portfolio/",
   plugins: [react(), svgr()],
 });
